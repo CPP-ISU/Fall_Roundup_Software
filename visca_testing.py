cbdfa3cb0896dcdb727071e37112316b841d20ac
dking1115 <davidking1115@me.com>
2023-10-10 16:03:25 -0500

    working on camera

--- visca_testing.py
+++ visca_testing.py
@@ -7,9 +7,18 @@ data_bits = 8
 stop_bits = 1
 parity = 'N'  # 'N' for None, 'E' for Even, 'O' for Odd
 
-ser = serial.Serial(port, baud_rate, data_bits, parity,stop_bits)
+ser = serial.Serial(port, baud_rate, data_bits, parity,stop_bits,timeout=1)
 
-command=b'8101060106050101FF'
-ser.write(command)
-response = ser.readline()
-print(response)
+#command=bytes.fromhex('81 01 06 01 18 18 03 01 FF')
+for i in range(1):
+    #command=bytes.fromhex('81 30 01 FF')
+    command=bytes.fromhex('81 01 06 01 18 18 03 02 FF')
+    #command=bytes.fromhex('81 2p FF')
+    #print(command)
+    ser.write(command)
+    response = ser.read(size=10)
+    print(response)
+
+
+
+ser.close()
