79cebcf0d8992b1a1d661afe0f051b349fbf0a69
dking1115 <davidking1115@me.com>
2023-10-20 09:26:29 -0500

    no more charts

--- ros_ws/src/camera/camera/submodules/camera_lib.py
+++ ros_ws/src/camera/camera/submodules/camera_lib.py
@@ -193,8 +193,10 @@ if __name__=="__main__":
 
     #cam.zoom_pos(0)
     #cam.zoom(-4)
-    cam.zoom_pos(50)
-    time.sleep(1)
+    #cam.zoom_pos(50)
+    #time.sleep(1)
+    cam.abs_pos(18,18,0,2
+                0)
     #cam.zoom(-7)
     #cam.exposure_set()
     #cam.focus_mode_set()
