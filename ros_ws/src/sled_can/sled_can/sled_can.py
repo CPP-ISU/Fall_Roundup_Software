65e85f02adc812f30ea74fd940de790c194f734c
david <davidking1115@me.com>
2023-10-20 02:05:52 -0500

    updating to only send when pull state is one

--- ros_ws/src/sled_can/sled_can/sled_can.py
+++ ros_ws/src/sled_can/sled_can/sled_can.py
@@ -13,13 +13,13 @@ class MyROSNode(Node):
         self.sled_pub = self.create_publisher(Sled,"sled",10)
         self.sled_timer=self.create_timer(.00001,self.callback_function)
         self.bus = can.Bus(channel='can0', interface='socketcan')
-        self.current_pull_pub= self.create_publisher(Currentpull,"current_pull",10)
+        self.current_pull_pub= self.create_publisher(Currentpull,"trackstate",10)
 
     def callback_function(self):
         message=self.bus.recv()
         #print(message.arbitration_id)
         if message.arbitration_id==0x0CFF6507:
-            print("msg")
+            #print("msg")
             self.pull_state=message.data[0]
             self.distance=(message.data[2]+message.data[3]<<8)*1.0
             self.force=(message.data[4]+message.data[5]<<8)/10.0
@@ -29,7 +29,8 @@ class MyROSNode(Node):
             sled.distance=self.distance
             sled.speed=self.speed
             self.pull_state=message.data[0]
-            self.sled_pub.publish(sled)
+            if self.pull_state==1:
+                self.sled_pub.publish(sled)
             current=Currentpull()
             current.trackstate=self.pull_state
             self.current_pull_pub.publish(current)
